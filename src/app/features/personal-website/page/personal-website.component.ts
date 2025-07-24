45ea9f5820bec71de44ed4c486b0dea58e550e40
Abdullah khormi <Abdullah.khormi@qeema.net>
2025-07-24 15:06:16 +0300

    add section about-me

--- src/app/features/personal-website/page/personal-website.component.ts
+++ src/app/features/personal-website/page/personal-website.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SummaryComponent } from "../../summary/summary.component";
+import { AboutMeComponent } from "../../about-me/about-me.component";
 
 @Component({
   selector: 'app-personal-website',
   standalone: true,
-  imports: [SummaryComponent],
+  imports: [SummaryComponent, AboutMeComponent],
   templateUrl: './personal-website.component.html',
   styleUrl: './personal-website.component.scss'
 })

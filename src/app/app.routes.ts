06320e912781dcab9276d4e098fbb8dc9f051bc8
Abdullah khormi <Abdullah.khormi@qeema.net>
2025-08-13 21:07:33 +0300

    change path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,16 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
-    {path:'',redirectTo:'/home', pathMatch: 'full'
-
-   },
-           {path:'home' ,loadComponent:()=>import('../../src/app/features/personal-website/page/personal-website.component').then(c=>c.PersonalWebsiteComponent)}
-];
+  {
+    path: '',
+    loadComponent: () =>
+      import('../../src/app/features/personal-website/page/personal-website.component')
+        .then(c => c.PersonalWebsiteComponent)
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('../../src/app/features/personal-website/page/personal-website.component')
+        .then(c => c.PersonalWebsiteComponent)
+  }
+]
